bb995c1c79c543f6be6b93b3aa8fd1f8adfbc8d3
disensee <isenseed@students.westerntc.edu>
2019-09-19 13:28:20 -0500

    Added step 2 instructions. Changed relative links to doc root relative links - finished step 2

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -2,7 +2,7 @@
 	<footer>
 		&copy; 201? YOUR NAME
 		&nbsp;&nbsp;&nbsp;
-		<a href="privacy-policy.php">Privacy Policy</a>
+		<a href="/my-new-site/privacy-policy.php">Privacy Policy</a>
 	</footer>
 	<div id="mobile-nav-button">&#9776;</div>
 </body>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -5,9 +5,9 @@
 	<meta charset="utf-8">
 	<meta name="description" content="<?php echo($pageDescription); ?>">
     <meta name="viewport" content="width=device-width">
-	<link rel="stylesheet" type="text/css" href="styles/reset.css">
-	<link rel="stylesheet" type="text/css" href="styles/main.css">
-	<script type="text/javascript" src="js/main.js"></script>
+	<link rel="stylesheet" type="text/css" href="/my-new-site/styles/reset.css">
+	<link rel="stylesheet" type="text/css" href="/my-new-site/styles/main.css">
+	<script type="text/javascript" src="/my-new-site/js/main.js"></script>
 </head>
 <body>
 	<header>
@@ -15,10 +15,10 @@
 	</header>
 	<nav id="main-nav">
 		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="pictures.php">Pictures</a></li>
-			<li><a href="blog/index.php">Blog</a></li>
-			<li><a href="contact.php">Contact</a></li>
+			<li><a href="/my-new-site/index.php">Home</a></li>
+			<li><a href="/my-new-site/pictures.php">Pictures</a></li>
+			<li><a href="/my-new-site/blog/index.php">Blog</a></li>
+			<li><a href="/my-new-site/contact.php">Contact</a></li>
 		</ul>
 	</nav>
 	<div id="content">
