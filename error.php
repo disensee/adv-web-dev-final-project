b32b575e497f02be0e1211e1ce5cbb272c35ae09
disensee <isenseed@students.westerntc.edu>
2019-10-03 14:39:24 -0500

    Finished step 5

---
+++ error.php
@@ -0,0 +1,24 @@
+<?php
+require_once("includes/config.inc.php");
+$pageTitle = "Error";
+$pageDescription = "Sorry. There was an error.";
+//$sideBar = "hobbies-sidebar.inc.php";
+require("includes/header.inc.php");
+?>
+		<main>
+
+			<div class="content-frame">
+				<h1>Sorry, there was an error.</h1>
+                <p>We have been notified and will fix it ASAP.</p>
+			</div>
+			
+		</main>
+<?php
+if(!empty($sideBar)){
+	require("includes/" . $sideBar);
+}
+
+require("includes/footer.inc.php")
+?>
+
+
